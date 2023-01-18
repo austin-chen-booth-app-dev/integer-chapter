49e9f4daf43cc19a7fc03db5a26011fc4c8bd2ed
austin-chen-booth <achen52@chicagobooth.edu>
2023-01-18 16:57:03 +0000

    Homework

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -1,9 +1,14 @@
 # Ask for a person’s age, and then calculate and display what year they were born in.
-# 
+#
 # Input:
 #   80
-# 
+#
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
-
 p "How old are you?"
+
+age = gets.chomp.to_i
+
+current_year = Time.now.year
+
+p("Wow, you were born in #{current_year - age}. You're old!")
